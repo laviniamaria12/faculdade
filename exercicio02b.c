e25515e955e236ee0f01c36fab3dbec901ac6869
laviniamaria12 <laviniamariam77@gmail.com>
2025-04-01 10:33:45 -0300

    Add files via upload

---
+++ exercicio02b.c
@@ -0,0 +1,45 @@
+#include <stdio.h> 
+
+void linha(); 
+
+ 
+
+int main() { 
+
+     
+
+    linha();  
+
+    return 0; 
+
+} 
+
+  
+
+void linha() { 
+
+    for (int i = 0; i < 25; i++) { 
+
+        printf("*"); 
+
+    } 
+
+    printf("\nCMP 1048\n"); 
+
+    for (int i = 0; i < 25; i++) { 
+
+        printf("@"); 
+
+    } 
+
+    printf("\nTécnicas de Programação\n"); 
+
+    for (int i = 0; i < 25; i++) { 
+
+        printf("+"); 
+
+    } 
+
+    printf("\n"); 
+
+} 
